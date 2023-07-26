5a43b80e9af17e701de71b60b1166dfc79e9172e
tonymtz <atony.mtz@gmail.com>
2023-07-26 12:52:53 -0700

    fixes sitemap not including static routes

--- src/app/sitemap.tsx
+++ src/app/sitemap.tsx
@@ -2,13 +2,27 @@ import { MetadataRoute } from 'next'
 
 import { getAllPosts } from '~/lib/api'
 
+const staticPages = [
+  '',
+  'ley-federal-del-trabajo',
+  'articulos',
+]
+
 export default function sitemap (): MetadataRoute.Sitemap {
   const pages = getAllPosts()
 
-  return pages.map(({ slug }) => ({
-    url: `https://eslegalmitrabajo.com/articulos/${ slug }`,
-    lastModified: new Date().toISOString(),
-    priority: 1,
-    changeFrequency: 'monthly',
-  }))
+  return [
+    ...staticPages.map((slug) => ({
+      url: `https://eslegalmitrabajo.com/${ slug }`,
+      lastModified: new Date().toISOString(),
+      priority: 1,
+      changeFrequency: 'monthly',
+    })),
+    ...pages.map(({ slug }) => ({
+      url: `https://eslegalmitrabajo.com/articulos/${ slug }`,
+      lastModified: new Date().toISOString(),
+      priority: 1,
+      changeFrequency: 'monthly',
+    })),
+  ]
 }
